e46f70aad7e2767e34c3281205527154d641ef49
Luohao Xu <luohaox@gmail.com>
2020-05-27 08:40:06 +0200

    Update split_bam.sh

--- assembly/split_bam.sh
+++ assembly/split_bam.sh
@@ -9,8 +9,8 @@
 #SBATCH --output=nuc-%j.out
 #SBATCH --error=nuc-%j.err
 
-cat  curated.fasta.fai.50k | cut -f 1|  while read line; do samtools view -@ 24 -h pacbioccs.fq.bam $line -O BAM -o $TMPDIR/pacbioccs.$line.bam; mv $TMPDIR/pacbioccs.$line.bam   pb/split/; samtools index -@ 24  pb/split/pacbioccs.$line.bam; done
+cat  curated.fasta.fai | cut -f 1|  while read line; do samtools view -@ 24 -h pacbioccs.fq.bam $line -O BAM -o $TMPDIR/pacbioccs.$line.bam; mv $TMPDIR/pacbioccs.$line.bam   pb/split/; samtools index -@ 24  pb/split/pacbioccs.$line.bam; done
 
-cat  curated.fasta.fai.50k | cut -f 1  | while read line; do samtools view -@ 24  -h hic/hic.fix.sort.md.bam $line -O BAM -o $TMPDIR/hic.$line.bam ; mv $TMPDIR/hic.$line.bam hic/split; samtools index -@ 24 hic/split/hic.$line.bam; done
+cat  curated.fasta.fai | cut -f 1  | while read line; do samtools view -@ 24  -h hic/hic.fix.sort.md.bam $line -O BAM -o $TMPDIR/hic.$line.bam ; mv $TMPDIR/hic.$line.bam hic/split; samtools index -@ 24 hic/split/hic.$line.bam; done
 
 #$HIC/hic.$SCAFFOLDS.bam
